dbbaaf075823e734783b92b9513706f085b44f84
Peter Grace <pete.grace@gmail.com>
2020-09-17 19:18:22 -0400

    Render template per route (#1)
    * workspace changes
    
    * changes to main.rs
    
    * per-route template config ability
    
    * updates

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
+function log() {
+    echo "$(date +%s) $*"
+}
 if [[ ! -z "${DEBUG}" ]]
 then
     echo "DEBUG requested, sleeping infinity."
     /bin/sleep infinity
 fi
+
+if [[ ! -d "/app/templates/dbrs-error-no-code.html.hbs" ]];
+then
+    log "a default template was not detected in templates folder, reseeding defaults into container filesystem."
+    cp -r /app/default_templates /app/templates
+    cp -r /app/default_public /app/public
+fi
+
 export ROCKET_ENV=${app_env:-prod}
 cd /app
 ./default-backend-rs
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,9 @@ mod metrics;
 extern crate serde;
 #[macro_use]
 extern crate rocket;
+#[macro_use]
+extern crate log;
+extern crate env_logger;
 use crate::all_headers::AllHeaders;
 use metrics::DEFAULT_BACKEND_APP_VER;
 use rocket::http::{Header, HeaderMap, Status};
@@ -16,6 +19,7 @@ use rocket_contrib::serve::StaticFiles;
 use rocket_contrib::templates::Template;
 use rocket_prometheus::{prometheus, PrometheusMetrics};
 use serde::Deserialize;
+use std::borrow::Borrow;
 use std::collections::HashMap;
 use std::error::Error;
 
@@ -23,33 +27,52 @@ static COMPRESSED_DEPENDENCY_LIST: &[u8] = auditable::inject_dependency_list!();
 
 #[get("/<file>")]
 fn get_error(file: String, headers: AllHeaders) -> Custom<Template> {
-    let context = headers;
     let mut variables = HashMap::new();
-    variables.insert("user_agent", context.get(String::from("User-Agent")));
-    variables.insert("status_code", context.get(String::from("X-Code")));
-    variables.insert("accept_header", context.get(String::from("X-Format")));
-    variables.insert("original_uri", context.get(String::from("X-Original-URI")));
+    variables.insert("user_agent", headers.get(String::from("User-Agent")));
+    variables.insert("status_code", headers.get(String::from("X-Code")));
+    variables.insert("accept_header", headers.get(String::from("X-Format")));
+    variables.insert("original_uri", headers.get(String::from("X-Original-URI")));
     variables.insert(
         "backend_namespace",
-        context.get(String::from("X-Namespace")),
+        headers.get(String::from("X-Namespace")),
     );
-    variables.insert("ingress_name", context.get(String::from("X-Ingress-Name")));
-    variables.insert("service_name", context.get(String::from("X-Service-Name")));
-    variables.insert("service_port", context.get(String::from("X-Service-Port")));
-    variables.insert("request_id", context.get(String::from("X-Request-ID")));
+    variables.insert("ingress_name", headers.get(String::from("X-Ingress-Name")));
+    variables.insert("service_name", headers.get(String::from("X-Service-Name")));
+    variables.insert("service_port", headers.get(String::from("X-Service-Port")));
+    variables.insert("request_id", headers.get(String::from("X-Request-ID")));
 
     let status_code: u16;
     match variables.get("status_code") {
-        Some(e) => status_code = e.unwrap_or(&String::from("500")).parse().unwrap_or(500),
-        None => status_code = 500,
+        Some(e) => status_code = e.unwrap_or(&String::from("1001")).parse().unwrap_or(1002),
+        None => status_code = 1000,
     }
-
-    let t = Template::render("default", &variables);
-    let s = Status::new(status_code, "reason");
-    status::Custom(s, t)
+    return match status_code {
+        1000 => {
+            let t: Template = Template::render("dbrs-error-no-code", &variables);
+            let s: Status = Status::new(status_code, "reason");
+            status::Custom(s, t)
+        }
+        1001 => {
+            let t: Template = Template::render("dbrs-error-no-code", &variables);
+            let s: Status = Status::new(500, "reason");
+            status::Custom(s, t)
+        }
+        1002 => {
+            let t: Template = Template::render("dbrs-error-parse", &variables);
+            let s: Status = Status::new(500, "reason");
+            status::Custom(s, t)
+        }
+        _ => {
+            let t: Template = Template::render("error", &variables);
+            let s = Status::new(status_code, "reason");
+            status::Custom(s, t)
+        }
+    };
 }
 
 fn main() {
+    std::env::set_var("RUST_LOG", "info,kube=info");
+    env_logger::init();
     let prometheus = PrometheusMetrics::new();
     prometheus
         .registry()
