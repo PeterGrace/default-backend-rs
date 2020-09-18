ecc9c39ed9daaa358de8188e4862d67e140fe3e9
Peter Grace <pete.grace@gmail.com>
2020-09-18 15:42:06 -0400

    send all headers all the time; install tests

--- src/main.rs
+++ src/main.rs
@@ -7,21 +7,17 @@ mod metrics;
 extern crate serde;
 #[macro_use]
 extern crate rocket;
-#[macro_use]
-extern crate log;
 extern crate env_logger;
+extern crate log;
 use crate::all_headers::AllHeaders;
 use metrics::DEFAULT_BACKEND_APP_VER;
-use rocket::http::{Header, HeaderMap, Status};
+use rocket::http::Status;
 use rocket::response::status;
 use rocket::response::status::Custom;
+use rocket::Rocket;
 use rocket_contrib::serve::StaticFiles;
 use rocket_contrib::templates::Template;
 use rocket_prometheus::{prometheus, PrometheusMetrics};
-use serde::Deserialize;
-use std::borrow::Borrow;
-use std::collections::HashMap;
-use std::error::Error;
 
 static COMPRESSED_DEPENDENCY_LIST: &[u8] = auditable::inject_dependency_list!();
 
@@ -32,55 +28,31 @@ fn get_health() -> String {
 
 #[get("/")]
 fn get_error(headers: AllHeaders) -> Custom<Template> {
-    let mut variables = HashMap::new();
-    variables.insert("user_agent", headers.get(String::from("User-Agent")));
-    variables.insert("status_code", headers.get(String::from("X-Code")));
-    variables.insert("accept_header", headers.get(String::from("X-Format")));
-    variables.insert("original_uri", headers.get(String::from("X-Original-URI")));
-    variables.insert(
-        "backend_namespace",
-        headers.get(String::from("X-Namespace")),
-    );
-    variables.insert("ingress_name", headers.get(String::from("X-Ingress-Name")));
-    variables.insert("service_name", headers.get(String::from("X-Service-Name")));
-    variables.insert("service_port", headers.get(String::from("X-Service-Port")));
-    variables.insert("request_id", headers.get(String::from("X-Request-ID")));
-
     let status_code: u16;
-    match variables.get("status_code") {
-        Some(e) => status_code = e.unwrap_or(&String::from("1001")).parse().unwrap_or(1002),
+    match headers.get(String::from("X-Code")) {
+        Some(e) => status_code = e.parse().unwrap_or(1001),
         None => status_code = 1000,
     }
     return match status_code {
         1000 => {
             let t: Template = Template::render("unknown", &headers);
-            let s: Status = Status::new(200, "reason");
+            let s: Status = Status::Ok;
             status::Custom(s, t)
         }
         1001 => {
-            let t: Template = Template::render("unknown", &headers);
-            let s: Status = Status::new(200, "reason");
-            status::Custom(s, t)
-        }
-        1002 => {
-            let t: Template = Template::render("dbrs-error-parse", &variables);
-            let s: Status = Status::new(500, "reason");
+            let t: Template = Template::render("dbrs-error-parse", &headers);
+            let s: Status = Status::InternalServerError;
             status::Custom(s, t)
         }
         _ => {
-            let t: Template = Template::render("error", &variables);
-            let s = Status::new(status_code, "reason");
+            let t: Template = Template::render("error", &headers);
+            let s = Status::new(status_code, "Unspecified");
             status::Custom(s, t)
         }
     };
 }
 
-fn main() {
-    match std::env::var("RUST_LOG") {
-        Err(_) => std::env::set_var("RUST_LOG", "warn"),
-        Ok(_) => ()
-    }
-    env_logger::init();
+fn prepare_rocket() -> Rocket {
     let prometheus = PrometheusMetrics::new();
     prometheus
         .registry()
@@ -90,11 +62,66 @@ fn main() {
         DEFAULT_BACKEND_APP_VER.with_label_values(&[env!("CARGO_PKG_VERSION"), env!("GIT_HASH")]);
     appdata_gauge.set(1.0);
     prometheus::gather();
+
     rocket::ignite()
         .attach(Template::fairing())
         .attach(prometheus.clone())
         .mount("/metrics", prometheus)
         .mount("/public", StaticFiles::from("public"))
         .mount("/", routes![get_error, get_health])
-        .launch();
+}
+
+fn main() {
+    match std::env::var("RUST_LOG") {
+        Err(_) => std::env::set_var("RUST_LOG", "debug"),
+        Ok(_) => (),
+    }
+    env_logger::init();
+
+    prepare_rocket().launch();
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use rocket::http::{Header, Status};
+    use rocket::local::Client;
+
+    #[test]
+    fn test_valid_x_code() {
+        let rocket = prepare_rocket();
+        let client = Client::new(rocket).unwrap();
+        let h = Header::new("X-Code", "418");
+        let response = client.get("/").header(h).dispatch();
+        assert_eq!(response.status(), Status::new(418, "Unspecified"));
+    }
+    #[test]
+    fn test_x_code_parse_error() {
+        let rocket = prepare_rocket();
+        let client = Client::new(rocket).unwrap();
+        let h = Header::new("X-Code", "foobar");
+        let response = client.get("/").header(h).dispatch();
+        assert_eq!(response.status(), Status::InternalServerError);
+    }
+    #[test]
+    fn test_no_x_code() {
+        let rocket = prepare_rocket();
+        let client = Client::new(rocket).unwrap();
+        let response = client.get("/").dispatch();
+        assert_eq!(response.status(), Status::Ok);
+    }
+    #[test]
+    fn test_metrics_fairing() {
+        let rocket = prepare_rocket();
+        let client = Client::new(rocket).unwrap();
+        let response = client.get("/metrics").dispatch();
+        assert_eq!(response.status(), Status::Ok);
+    }
+    #[test]
+    fn test_staticfile_fairing() {
+        let rocket = prepare_rocket();
+        let client = Client::new(rocket).unwrap();
+        let response = client.get("/public/bar").dispatch();
+        assert_eq!(response.status(), Status::Ok);
+    }
 }
